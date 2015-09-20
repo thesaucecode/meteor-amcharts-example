553961dccb32e1a646c7002a5cd912e46f2de573
Joel Spira <joeldspira@gmail.com>
2015-09-20 12:17:58 +1000

    jplu siteupdate

--- router.js
+++ router.js
@@ -1,18 +1,22 @@
+Router.configure({
+	layoutTemplate: 'layout'
+});
+
 Router.map(function () {
   this.route('home', {
     path: '/'
   });
 
   this.route('example1', {
-
+  	
   });
 
   this.route('example2', {
-
+  	
   });
 
   this.route('example3', {
-
+  	
   });
 
 });
